a9a1b84d7143cb895f9cd54f144fded1c66d42f1
Brian Maselli <bmaselli@Brians-M2-MacBook-Air.local>
2023-07-21 08:00:06 -0700

    Initialization of code on GitHub

---
+++ Simulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyTransfer
+{
+    internal class Simulation
+    {
+        public Simulation()
+        {
+            
+        }
+
+        public void RunSimulation()
+        {
+            Console.WriteLine("Starting simulation");
+            //TODO 5: Instantiate two Person objects, with unique names and money amounts. Two lines.
+            int myNumber = 5 ;
+            Person person1 = new Person("Brian", 1000);
+            Person person2 = new Person("Joan", 500);
+
+            //TODO 6: Call the .DisplayInfo() method on each person object, one at a time. This will show their initial money amount. Two lines.
+            person1.DisplayInfo();
+            person2.DisplayInfo();
+
+            //TEST THE APPLICATION AFTER TODOS 5 AND 6!!
+
+            //TODO 9: Call the .TransferMoney() method on one person object (giver), making sure to pass in the second person object (receiver), and the amount being transfered. One line.
+            person1.TransferMoney(person2, 250);
+
+
+            //TODO 10: Call the .DisplayInfo() method on each person object again, showing that each person's balance has changed. Two lines.
+            person1.DisplayInfo();
+            person2.DisplayInfo();
+
+
+            Console.WriteLine("Simulation complete.");
+        }
+    }
+}
